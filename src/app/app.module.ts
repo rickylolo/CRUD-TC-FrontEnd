b821ace04dfd0404e6cb214ce6e61dd86eee8021
rickylolo <83848895+rickylolo@users.noreply.github.com>
2023-07-21 10:34:33 -0600

    CreditCardComponent

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreditCardComponent } from './credit-card/credit-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreditCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
